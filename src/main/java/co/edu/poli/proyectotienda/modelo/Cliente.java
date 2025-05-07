b3f0ec897b1bcc2fdd4fa137eb21983f9a0c4921
Sebastian Barragan <sbarraganb@poligran.edu.co>
2025-05-07 18:18:03 -0500

    Avance actividad 11

--- src/main/java/co/edu/poli/proyectotienda/modelo/Cliente.java
+++ src/main/java/co/edu/poli/proyectotienda/modelo/Cliente.java
@@ -1,5 +1,52 @@
 package co.edu.poli.proyectotienda.modelo;
 
 public class Cliente {
-    
+    private int id;
+    private String nombre;
+    private String email;
+    private String direccion;
+
+    public Cliente(int id, String nombre, String email, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + email + ")";
+    }
 }
--- src/main/java/co/edu/poli/proyectotienda/modelo/Producto.java
+++ src/main/java/co/edu/poli/proyectotienda/modelo/Producto.java
@@ -1,5 +1,61 @@
 package co.edu.poli.proyectotienda.modelo;
 
 public class Producto {
-        
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(int id, String nombre, double precio, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean reducirStock(int cantidad) {
+        if (stock >= cantidad) {
+            stock -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - $" + precio;
+    }
 }
+
