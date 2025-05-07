cfd1acc7c5f6337c62be55a1242d5ae7a4a57227
Sebastian Barragan <sbarraganb@poligran.edu.co>
2025-05-07 11:58:26 -0500

    Initial commit actividad 11

---
+++ src/main/java/co/edu/poli/proyectotienda/vista/App.java
@@ -0,0 +1,42 @@
+package co.edu.poli.proyectotienda.vista;
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+    	AnchorPane main = (AnchorPane) FXMLLoader.load(getClass().getResource("/co/edu/poli/proyectotienda/vista/formulario.fxml"));
+        scene = new Scene(main);
+        stage.setScene(scene);
+        stage.setTitle("Tienda");
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
